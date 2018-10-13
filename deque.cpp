62152160673b8cf7bed6f34d7e185cfffa2b717e
sirjansingh310 <sirjansingh310@gmail.com>
2018-10-13 08:41:29 -0700

    Create deque.cpp

---
+++ deque.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <deque> 
+using namespace std;
+// sliding window maximum. for ref : https://www.geeksforgeeks.org/sliding-window-maximum-maximum-of-all-subarrays-of-size-k/
+void printKMax(int arr[], int n, int k){
+	deque<int> dq;
+    for(int i=0;i<k;i++){
+        while(!dq.empty() && arr[i]>= arr[dq.back()])
+            dq.pop_back();
+        dq.push_back(i);
+    }
+    for(int i=k;i<n;i++){
+        cout << arr[dq.front()] << " ";
+        while(!dq.empty() && dq.front() <=i-k)
+            dq.pop_front();
+        while(!dq.empty() && arr[i]>=arr[dq.back()])
+            dq.pop_back();
+        dq.push_back(i);
+    }
+    cout << arr[dq.front()] << endl;
+}
+
+int main(){
+  
+	int t;
+	cin >> t;
+	while(t>0) {
+		int n,k;
+    	cin >> n >> k;
+    	int i;
+    	int arr[n];
+    	for(i=0;i<n;i++)
+      		cin >> arr[i];
+    	printKMax(arr, n, k);
+    	t--;
+  	}
+  	return 0;
+}
